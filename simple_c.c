815f5b7f704634cd2c825ed8431ea73001c95ef5
nkrishna <neelima.krishnan@intel.com>
2024-04-12 11:34:58 -0600

    Add simple program to add numbers

---
+++ simple_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10, num2 = 20, sum;
+
+    // Inline assembly syntax
+    __asm__("addl %%ebx, %%eax;" // Assembly instruction
+            : "=a" (sum)        // Output operands
+            : "a" (num1), "b" (num2) // Input operands
+            );
+
+    printf("Sum is: %d\n", sum);
+    return 0;
+}
+
